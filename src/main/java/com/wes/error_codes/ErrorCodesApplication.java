a6aad938a84bc60a18b3373b169b743ba2320e8b
AdelaideBaron <baronadelaide@hotmail.com>
2023-12-29 10:57:24 +0000

    initial commit

---
+++ src/main/java/com/wes/error_codes/ErrorCodesApplication.java
@@ -0,0 +1,13 @@
+package com.wes.error_codes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorCodesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrorCodesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wes/error_codes/ErrorCodesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wes.error_codes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorCodesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
