1003e2b87ab79239ce4945b676d12b7a9810ee9f
Adelaide-Baron <addi.baron@currencycloud.com>
2024-01-14 13:13:26 +0000

    possible causes objects

---
+++ src/main/java/com/wes/error_codes/PossibleCause.java
@@ -0,0 +1,12 @@
+package com.wes.error_codes;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class PossibleCause {
+
+    private final String cause;
+
+}
