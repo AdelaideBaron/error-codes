7bb578cf82f9a8de0b2bd45e8da33e68d4b3308a
Adelaide-Baron <addi.baron@currencycloud.com>
2024-02-01 23:39:44 +0000

    adding helper methods

--- src/main/java/com/wes/error_codes/util/ConfigHelper.java
+++ src/main/java/com/wes/error_codes/util/ConfigHelper.java
@@ -1,26 +1,41 @@
 package com.wes.error_codes.util;
 
 import com.wes.error_codes.model.Error;
+import com.wes.error_codes.model.Machine;
 import com.wes.error_codes.model.PossibleCause;
 import com.wes.error_codes.reader.ErrorCodeReader;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
 
 import java.util.List;
+import java.util.Optional;
+
 @Slf4j
 public class ConfigHelper { // parser? look at naming conventions for this...
 
-    public static List<String> errorCodes;
-    public static List<PossibleCause> possibleCauses;
+    public static List<Error> getErrorsForMachine(Machine machine){
+        return ErrorCodeReader.machineErrors.get(machine);
+    }
 
-    @Bean
-    public List<String> getErrorNamesFromConfig(){
-        List<String> errors = ErrorCodeReader.errorsFromCSV
-                .stream()
+    public static List<String> getErrorCodesForMachine(Machine machine){
+        return ErrorCodeReader.machineErrors.get(machine).stream()
                 .map(Error::getErrorCode)
                 .toList();
-        errorCodes = errors;
-        return errors;
+    }
+
+    public static List<PossibleCause> getPossibleCausesForError(Error errorToFind, Machine machine){
+        List<Error> errors =ErrorCodeReader.machineErrors.get(machine);
+
+        Optional<Error> foundError = errors.stream()
+                .filter(error -> error.getErrorCode().equals(errorToFind))
+                .findFirst();
+
+        if (foundError.isPresent()) {
+            Error error = foundError.get();
+            return error.getPossibleCauses();
+        } else {
+            log.error("Error not found: " + errorToFind);
+        }
+        return null;
     }
 
 }
