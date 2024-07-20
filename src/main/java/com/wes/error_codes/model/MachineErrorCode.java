a46678aeb53307aa03ffc9cc601e8d13383a40b4
AdelaideBaron <baronadelaide@hotmail.com>
2024-07-20 20:59:42 +0100

    updating models for CSV

--- src/main/java/com/wes/error_codes/model/MachineErrorCode.java
+++ src/main/java/com/wes/error_codes/model/MachineErrorCode.java
@@ -1,9 +1,14 @@
 package com.wes.error_codes.model;
 
 import java.util.List;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@Getter
+@RequiredArgsConstructor
 public class MachineErrorCode {
-    private String errorCode;
-    private String errorDetails;
-    private List<String> possibleCauses;
+  private final String errorCode;
+  private final String errorDetails;
+  private final List<String> possibleCauses;
+  private final String machine;
 }
