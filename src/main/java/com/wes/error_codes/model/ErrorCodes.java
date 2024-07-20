3844731567cdadb96b8932a7cd64216e023f9701
AdelaideBaron <baronadelaide@hotmail.com>
2024-07-20 19:56:23 +0100

    add csv model classes

---
+++ src/main/java/com/wes/error_codes/model/ErrorCodes.java
@@ -0,0 +1,7 @@
+package com.wes.error_codes.model;
+
+import java.util.List;
+
+public class ErrorCodes {
+    List<MachineErrorCode> machineErrorCodes;
+}
---
+++ src/main/java/com/wes/error_codes/model/MachineErrorCode.java
@@ -0,0 +1,9 @@
+package com.wes.error_codes.model;
+
+import java.util.List;
+
+public class MachineErrorCode {
+    private String errorCode;
+    private String errorDetails;
+    private List<String> possibleCauses;
+}
